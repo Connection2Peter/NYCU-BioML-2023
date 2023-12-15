bf91bf32bd021dd1bd56e6c4b01f8d0631ce538f
junjie1999 <junjie9931@gmail.com>
2023-12-16 04:29:32 +0800

    v231216_1629 - add more classifier

--- lib/rocplot.py
+++ lib/rocplot.py
@@ -52,6 +52,6 @@ class Roc_curve:
         plt.ylim([0.0, 1.0])
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate')
-        plt.title('Receiver Operating Characteristic (ROC) Curve for Versicolor')
+        plt.title('ROC Curve')
         plt.legend(loc='lower right')
         plt.show()
